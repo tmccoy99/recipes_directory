45e329ed6ae0100f889a07e04ac1bb471e81b15e
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-04 16:35:48 +0000

    Added planning file to gitignore and created test seed file

---
+++ spec/seeds.sql
@@ -0,0 +1,4 @@
+TRUNCATE TABLE recipes RESTART IDENTITY;
+
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Chicken Korma', 60, 4);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Fajitas', 25, 2);

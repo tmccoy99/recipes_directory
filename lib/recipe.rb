e85ef53dcd6262558d5d4127b0cc144cd57b33aa
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-04 17:13:35 +0000

    Added definition of equality for testing purposes

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -4,4 +4,11 @@ class Recipe
   def initialize(name = nil, cooking_time = nil, rating = nil, id = nil)
     @name = name ; @cooking_time = cooking_time ; @rating = rating ; @id = id
   end
+
+  def ==(other)
+    [@name, @cooking_time, @rating, @id] == [
+      other.name, other.cooking_time, other.rating, other.id
+    ]
+  end
+  
 end
